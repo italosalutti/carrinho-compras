ee14a99e9c8c834bb868ea4791e197f9db52302b
Italo Salutti <salutti@gmail.com>
2024-09-17 15:14:16 -0300

    adicionando itens ao carrinho

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 function adicionar(){
     /* 
     - recuperar valores - nome produto, quantidade, valor
-    - calcular preço, subtotal
-    - adicionar ao carrinho
+    
+    
     - atualizar valor total da compra
     */
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('- R$')[1];
-    let quantidadeProduto = document.getElementById('quantidade');
-    let valorProduto = document.getElementById()
-    let preco = quantidadeProduto.value * valorUnitario;
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidadeProduto.value);
-    alert(preco);
+    let valorUnitario = produto.split('R$')[1];
+    let quantidadeProduto = document.getElementById('quantidade').value;
+
+    //- calcular preço, subtotal
+    let preco = quantidadeProduto * valorUnitario;
+
+    //- adicionar ao carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidadeProduto}x</span> ${nomeProduto} <span class="texto-azul">R$${valorUnitario}</span>
+        </section>`
 }
 
 function limpar(){
